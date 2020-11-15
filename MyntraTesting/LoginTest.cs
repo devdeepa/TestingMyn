f19c0c3bce4a11e872d182ecf64106807d8f2cf7
deepak <deepakchopade14@gmail.com>
2020-11-15 11:34:18 +0530

    Initial commit

---
+++ MyntraTesting/LoginTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.IE;
+
+namespace MyntraTesting
+{
+    [TestFixture]
+    public class LoginTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            InternetExplorerDriverService driverService = InternetExplorerDriverService.CreateDefaultService(@"D:\Setups\IEDriverServer_x64_3.14.0\");
+            IWebDriver driver = new InternetExplorerDriver(driverService);
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.myntra.com";
+
+            
+        }
+    }
+}
---
+++ MyntraTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyntraTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyntraTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("53e5e606-b841-426b-b034-760c169c6500")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
